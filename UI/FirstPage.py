48cff09226b6224cf0b6445a798bc9c0d0dd7c5b
Makis <makis_kovanidis@hotmail.com>
2018-09-21 10:58:14 +0300

    some changes

--- UI/FirstPage.py
+++ UI/FirstPage.py
@@ -1,22 +1,7 @@
 import tkinter as tk
+import time
 
 root = tk.Tk()
-
-# topFrame = tk.Frame(root)
-# topFrame.pack()
-#
-# bottomFrame = tk.Frame(root)
-# bottomFrame.pack(side=tk.BOTTOM)
-#
-# button1 = tk.Button(topFrame, text="Button 1", fg="red")
-# button2 = tk.Button(topFrame, text="Button 2", fg="blue")
-# button3 = tk.Button(topFrame, text="Button 3", fg="green")
-# button4 = tk.Button(bottomFrame, text="Button 4", fg="red")
-#
-# button1.pack()
-# button2.pack()
-# button3.pack()
-# button4.pack()
 
 # labelFrame1 = LabelFrame
 labelFrame1 = tk.LabelFrame(root, text="hello")
@@ -29,11 +14,14 @@ tk.Label(labelFrame1, text="label_3").grid(column=4, row=4, sticky=tk.W)
 labelFrame2 = tk.LabelFrame(root, text="hello2")
 labelFrame2.grid(column=1, row=1, padx=20, pady=40 )
 
-tk.Label(labelFrame2, text="label_4").grid(column=0, row=0, sticky=tk.W)
+w = tk.Label(labelFrame2, text="label_4", font="Courier 20").grid(column=0, row=0, pady=5, sticky=tk.W)
+
 tk.Label(labelFrame2, text="label_5").grid(column=1, row=0, sticky=tk.W)
-tk.Label(labelFrame2, text="label_6").grid(column=4, row=4, sticky=tk.W)
+tk.Label(labelFrame2, text="label_6").grid(column=4, row=4, pady=5, sticky=tk.W)
 
+time.sleep(2)
 
+w['text'] = "New text"
 
 root.state('zoomed')
 root.resizable(False, False)
